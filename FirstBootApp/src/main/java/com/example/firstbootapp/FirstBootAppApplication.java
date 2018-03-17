dc89f7372b8c8e8711b0180df56a1cd0332e3cca
djordje <przulj@gmail.com>
2018-03-17 19:33:51 +0100

    Projekat pripremljen za pakovanje u war.

--- FirstBootApp/src/main/java/com/example/firstbootapp/FirstBootAppApplication.java
+++ FirstBootApp/src/main/java/com/example/firstbootapp/FirstBootAppApplication.java
@@ -4,17 +4,24 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class FirstBootAppApplication {
+public class FirstBootAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(FirstBootAppApplication.class, args);
 		String[] beanNames = ctx.getBeanDefinitionNames();
 		Arrays.sort(beanNames);
 		for (String beanName : beanNames) 
-		  System.out.println(beanName);
-
+			System.out.println(beanName);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+		return app.sources(FirstBootAppApplication.class);
+	}
+
 }
