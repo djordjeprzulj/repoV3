ad327d7972dd4ec4e9f007a601624b0f65ea2135
djordje <przulj@gmail.com>
2018-03-17 19:20:02 +0100

    Dodata metoda home.

--- FirstBootApp/src/main/java/com/example/firstbootapp/controllers/FirstMvcController.java
+++ FirstBootApp/src/main/java/com/example/firstbootapp/controllers/FirstMvcController.java
@@ -1,8 +1,15 @@
 package com.example.firstbootapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FirstMvcController {
 
+	@RequestMapping("/")
+	@ResponseBody
+	public String home() {
+		return "First MVC Controller";
+	}
 }
