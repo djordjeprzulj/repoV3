23e8327c269dfcc4a3b9f097a8d0e6774a13d3e4
djordje <przulj@gmail.com>
2018-03-17 19:20:50 +0100

    Dodata metoda data.

--- FirstBootApp/src/main/java/com/example/firstbootapp/controllers/FirstMvcController.java
+++ FirstBootApp/src/main/java/com/example/firstbootapp/controllers/FirstMvcController.java
@@ -1,5 +1,10 @@
 package com.example.firstbootapp.controllers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,4 +17,24 @@ public class FirstMvcController {
 	public String home() {
 		return "First MVC Controller";
 	}
+	
+	@RequestMapping("/data")
+	@ResponseBody
+	public String data() {
+		String rez = "";
+		try {
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/intens?user=root&password=roottt");
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery("select name "
+										+ "from t_product");
+			while (rs.next()) 
+				rez = rez + rs.getString(1) + "<br>";
+			rs.close();
+			st.close();
+	   } catch (Exception e) {
+		   e.printStackTrace();
+	   }
+	   return rez;
+	}	
 }
